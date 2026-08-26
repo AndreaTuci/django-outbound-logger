0228e47672244dfa98fb08c895c6faa633820e31
Andrea Tuci <tuci.registrazioni@gmail.com>
2026-08-26 11:42:16 +0200

    test: cover the preview and the admin classes used as mixins

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from outbound_logger.http.models import HttpRequestLog
 from outbound_logger.mail.models import EmailLog
 
-from .base import MailTestCase
+from .base import MailTestCase, build_message
 from .stubs import build_session
 
 CHANGELIST_URL = reverse("admin:outbound_mail_emaillog_changelist")
@@ -88,3 +88,70 @@ class HttpRequestLogAdminTests(MailTestCase):
 
         self.assertContains(response, "&quot;ok&quot;: true")  # the response body
         self.assertNotContains(response, 'name="url"')
+
+
+class HtmlPreviewTests(MailTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.staff = User.objects.create_superuser("look", "look@example.com", "secret")
+
+    def setUp(self):
+        self.client.force_login(self.staff)
+
+    def detail_of(self, message):
+        message.send()
+        log = EmailLog.objects.latest("pk")
+        url = reverse("admin:outbound_mail_emaillog_change", args=[log.pk])
+        return self.client.get(url)
+
+    def test_an_html_body_is_shown_in_a_sandboxed_frame(self):
+        message = build_message()
+        message.attach_alternative("<p>Ciao</p>", "text/html")
+
+        response = self.detail_of(message)
+
+        self.assertContains(response, "<iframe sandbox")
+        self.assertContains(response, "&lt;p&gt;Ciao&lt;/p&gt;")  # inside srcdoc
+
+    def test_the_preview_cannot_smuggle_markup_into_the_admin(self):
+        message = build_message()
+        message.attach_alternative("<script>alert(1)</script>", "text/html")
+
+        response = self.detail_of(message)
+
+        self.assertNotContains(response, "<script>alert(1)</script>")
+
+    def test_a_message_without_html_says_so(self):
+        response = self.detail_of(build_message())
+
+        self.assertContains(response, "no HTML body")
+
+
+class CustomisedAdminTests(MailTestCase):
+    """Projects on django-unfold and friends re-register with their own base."""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.staff = User.objects.create_superuser("boss2", "boss2@example.com", "secret")
+
+    def test_the_admin_classes_work_as_mixins(self):
+        from django.contrib import admin
+
+        from outbound_logger.mail.admin import EmailLogAdmin
+
+        class TheirModelAdmin(admin.ModelAdmin):
+            """Stands in for the ModelAdmin a theme ships."""
+
+        class TheirEmailLogAdmin(EmailLogAdmin, TheirModelAdmin):
+            pass
+
+        admin.site.unregister(EmailLog)
+        admin.site.register(EmailLog, TheirEmailLogAdmin)
+        self.addCleanup(admin.site.register, EmailLog, EmailLogAdmin)
+        self.addCleanup(admin.site.unregister, EmailLog)
+
+        build_message().send()
+        self.client.force_login(self.staff)
+        response = self.client.get(reverse("admin:outbound_mail_emaillog_changelist"))
+
+        self.assertContains(response, "to@example.com")
